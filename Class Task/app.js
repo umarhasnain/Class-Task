cd6961bbd6621bb953863201b56d1d849a2a4800
Umar Hasnain <103918550+umarhasnain@users.noreply.github.com>
2023-03-15 20:26:32 +0500

    Add files via upload

---
+++ Class Task/app.js
@@ -0,0 +1,39 @@
+//**************************CLASS TASK *****************************/
+
+//Lower Case
+// var str = " umar";
+// console.log(str.toUpperCase())
+
+// var str = "UMAR";
+// console.log(str.toLowerCase())
+
+// var str = prompt("Enter a Name");
+// // var str = "muhammad umar hasnain";
+// var words = str.split(" ");
+// var arr = []
+
+//Upper Case
+
+// for(var i = 0; i < words.length; i++){
+//     var result = words[i][0].toUpperCase() + words[i].slice(1)
+//     arr.push(result);
+    
+// }
+// console.log(arr.join(" "))
+
+
+// Replace Logic
+
+// var str = "mass it training saylani";
+// for(var i = 0; i < str.length; i++){
+//     if(str.slice(i, i + 7 ) == "saylani"){
+//         str = str.slice(0,i)+ "SMIT" + str. slice(i + 7)
+//     }
+// }
+// console.log(str)
+
+
+//Replace Method
+
+// var str = "saylani mass it training";
+// console.log(str.replaceAll("saylani" , "SMIT"))
